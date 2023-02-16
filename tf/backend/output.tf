16a5d99f5e266953e1d1a8b73e0f0ef905c6d6a5
josephmienko <mienko@uw.edu>
2023-02-16 10:49:54 -0800

    giving up on tf native backend for now

--- tf/backend/output.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_id" {
-  value       = module.s3_bucket.bucket_id
-  description = "Bucket ID"
-}
-
-output "bucket_arn" {
-  value       = module.s3_bucket.bucket_arn
-  description = "Bucket ARN"
-}
--- tf/main.tf
+++ tf/main.tf
@@ -1,15 +1,81 @@
+data "aws_caller_identity" "this" {}
 
-data "terraform_remote_state" "backend" {
-  backend = "s3"
-  config = {
-    bucket = module.backend.bucket_id
-    key    = "backend/terraform.tfstate"
-    region = "us-west-1"
+# This will be the assume role policy for all SSO roles
+data "aws_iam_policy_document" "this" {
+  statement {
+    actions = ["sts:AssumeRoleWithSAML"]
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_saml_provider.this.arn]
+    }
+    condition {
+      test     = "StringEquals"
+      variable = "SAML:aud"
+      values   = ["https://signin.aws.amazon.com/saml"]
+    }
   }
 }
 
-module "backend" {
-  source = "./backend"
+# The SAML provider for the account needs the IdP's metadata
+# This only contains the public key and is OK to check in
+resource "aws_iam_saml_provider" "this" {
+  name                   = "UW"
+  saml_metadata_document = file("${path.module}/idp-metadata.xml")
+}
 
-  bucket_id = module.backend.bucket_id
+# Role definitions
+
+resource "aws_iam_role" "cssat-data-engineer" {
+  name               = "cssat-data-engineer"
+  assume_role_policy = data.aws_iam_policy_document.this.json
+}
+
+resource "aws_iam_role_policy_attachment" "cssat-data-engineer" {
+  role       = aws_iam_role.cssat-data-engineer.name
+  policy_arn = "arn:aws:iam::aws:policy/cssat-data-engineer-policy"
+}
+
+resource "aws_iam_role" "cssat-aws-admin" {
+  name               = "cssat-aws-admin"
+  assume_role_policy = data.aws_iam_policy_document.this.json
+}
+
+resource "aws_iam_role_policy_attachment" "cssat-aws-admin" {
+  role       = aws_iam_role.cssat-aws-admin.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "cssat-aws-admin" {
+  role       = aws_iam_role.cssat-aws-admin.name
+  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
+}
+
+resource "aws_iam_role_policy_attachment" "cssat-aws-admin" {
+  role       = aws_iam_role.cssat-aws-admin.name
+  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "cssat-aws-admin" {
+  role       = aws_iam_role.cssat-aws-admin.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "cssat-aws-admin" {
+  role       = aws_iam_role.cssat-aws-admin.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "cssat-aws-admin" {
+  role       = aws_iam_role.cssat-aws-admin.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonRDSDataFullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "cssat-aws-admin" {
+  role       = aws_iam_role.cssat-aws-admin.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "cssat-aws-admin" {
+  role       = aws_iam_role.cssat-aws-admin.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonVPCFullAccess"
 }
